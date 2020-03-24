5df9aeddab7d5660a0408600c6b806a6a5864ea0
unknown <aja.washington13@gmail.com>
2020-03-24 16:04:51 -0500

    pathsCorrected

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using DepartmentsEmployeesConsole.Data;
+using DepartmentsEmployeesConsole.Models;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System;
+
+namespace DepartmentsEmployeesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DepartmentRepository departmentRepo = new DepartmentRepository();
+
+            Console.WriteLine("Getting All Departments:");
+            Console.WriteLine();
+
+            List<Department> allDepartments = departmentRepo.GetAllDepartments();
+
+            foreach (Department dept in allDepartments)
+            {
+                Console.WriteLine($"{dept.Id} {dept.DeptName}");
+            }
+
+
+            EmployeeRepository employeeRepo = new EmployeeRepository();
+      
+
+            Console.WriteLine("Getting All Employees:");
+            Console.WriteLine();
+
+            List<Employee> allEmployees = employeeRepo.GetAllEmployees();
+
+            foreach (Employee emp in allEmployees)
+            {
+                Console.WriteLine($"{emp.Id} {emp.FirstName}");
+            }
+
+
+
+        }
+    }
+}
